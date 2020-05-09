332073af4b956efceeef25e4e8fa1f134968c5fd
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-05-08 20:03:35 -0700

    basics of the app work
    the x button only works once

--- public_api_request-v1/app.js
+++ public_api_request-v1/app.js
@@ -1,15 +1,55 @@
 //fetch functions
 //api request URL https://randomuser.me/api/?results=12
 
+
+
+
+
+
+// let testData; 
+
 fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
-    //.then(data => console.log(data.results[0].email))
     .then(data => {
+        console.log(data);
         for (let i = 0; i < data.results.length; i++ ) {
-        createCardDiv(data.results[i].picture.medium, data.results[i].name.first, data.results[i].name.last, data.results[i].email, data.results[i].location.city, data.results[i].location.state)
+           createCardDiv(data.results[i].picture.medium, data.results[i].name.first, data.results[i].name.last, data.results[i].email, data.results[i].location.city, data.results[i].location.state)
         }
-    })
-    .then(cards => {document.querySelector('.card').addEventListener('click', createModal)})
+           let cards = document.getElementsByClassName('card')
+             console.log(cards)
+            for (let j = 0; j<cards.length; j++){
+            cards[j].addEventListener('click', (event) => {
+            createModal(data.results[j].picture.large, data.results[j].name.first, data.results[j].name.last, 
+                data.results[j].email, data.results[j].location.city, data.results[j].cell, 
+                data.results[j].location.street.number, data.results[j].location.street.name, data.results[j].location.state, 
+                data.results[j].location.postcode, data.results[j].dob.date.substring(0, 10));
+
+                let xBtn = document.getElementById('modal-close-btn');
+                        console.log(xBtn);
+                
+                        xBtn.addEventListener('click', (e) => {
+                           let modalContainer = document.querySelector('.modal-container');
+                           modalContainer.style.display = 'none';
+                        });
+                    
+
+
+             })
+
+             
+
+            }
+            
+    
+ })
+
+
+ 
+
+
+        //   document.querySelector('.card').addEventListener('click', (event) => {
+        //     createModal(cards.results.medium);
+    //url, firstName, lastName, email, city, phoneNumber, number, street, state, zip, birthday
     // .then(modalWindow => {
     //     let xBtn = document.getElementById('modal-close-btn');
     //         console.log(xBtn);
@@ -18,8 +58,7 @@ fetch('https://randomuser.me/api/?results=12')
     //         });
 
     // })
-    
-    //.then(items => JSON.parse(items))
+
 
 //helper functions to create HTML
 
@@ -94,8 +133,8 @@ function createCardDiv(url, firstName, lastName, email, city, state) {
 //placeholder function call for testing
 //createCardDiv();
 
-
-function createModal() {
+//, firstName, lastName, email, city, phoneNumber, number, street, state, zip, birthday
+function createModal(url, firstName, lastName, email, city, phoneNumber, number, street, state, zip, birthday) {
     //child of body
     const modalDiv = document.createElement('div');
     document.body.appendChild(modalDiv);
@@ -116,14 +155,14 @@ function createModal() {
 
     const infoContainer = document.createElement('div');
     infoContainer.setAttribute('class', 'modal-info-container');
-    modalDiv.appendChild(infoContainer);
+    modalDivTwo.appendChild(infoContainer);
 
     //everything else needs to be appended to infoContainer
     const img = document.createElement('img');
     infoContainer.appendChild(img);
     img.setAttribute('class', 'modal-img');
     //needs the url from the api
-    img.setAttribute('src', 'https://placehold.it/125x125');
+    img.setAttribute('src', url);
     img.setAttribute('alt', 'profile picture');
 
     //h3
@@ -131,34 +170,40 @@ function createModal() {
     infoContainer.appendChild(nameH3);
     nameH3.setAttribute('id', 'name');
     nameH3.setAttribute('class', 'modal-name cap');
-    nameH3.textContent = 'name';
+    nameH3.textContent = `${firstName} ${lastName}`;
+    //nameH3.textContent = 'name';
  
     const emailP = document.createElement('p');
     infoContainer.appendChild(emailP);
     emailP.setAttribute('class', 'modal-text');
-    emailP.textContent = 'email';
+    emailP.textContent = `${email}`;
+    //emailP.textContent = 'email';
 
     const cityP = document.createElement('p');
     infoContainer.appendChild(cityP);
     cityP.setAttribute('class', 'modal-text cap');
-    cityP.textContent = 'city';
+    cityP.textContent = `${city}`;
+    //cityP.textContent = 'city';
 
     infoContainer.appendChild(document.createElement('hr'));
 
     const phoneP = document.createElement('p');
     infoContainer.appendChild(phoneP);
     phoneP.setAttribute('class', 'modal-text');
-    phoneP.textContent = 'phone number';
+    phoneP.textContent = `${phoneNumber}`;
+    //phoneP.textContent = 'phoneNumber';
 
     const addressP = document.createElement('p');
     infoContainer.appendChild(addressP);
     addressP.setAttribute('class', 'modal-text');
-    addressP.textContent = 'address';
+    addressP.textContent = `${number} ${street}, ${state} ${zip}`;
+    //addressP.textContent = 'address';
 
     const birthdayP = document.createElement('p');
     infoContainer.appendChild(birthdayP);
     birthdayP.setAttribute('class', 'modal-text');
-    birthdayP.textContent = 'Birthday: birthday';
+    birthdayP.textContent = `Birthday: ${birthday}`;
+    //birthdayP.textContent = 'birthday';
 
 
 }
